a83e3d1c0ddfc851e41d65d411043fdeed21d462
EC2 Default User <ec2-user@ip-172-31-23-130.ec2.internal>
2017-09-21 14:35:28 +0000

    apache cookbook added

---
+++ apache/test/recipes/server.rb
@@ -0,0 +1,51 @@
+# # encoding: utf-8
+
+# Inspec test for recipe apache::server
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at https://docs.chef.io/inspec_reference.html
+
+unless os.windows?
+  describe user('root') do
+    it { should exist }
+    skip 'This is an example test, replace with your own test.'
+  end
+end
+
+describe package('httpd') do
+  it { should be_installed }
+end
+
+describe service('httpd') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe 'configuration' do
+  let(:config) { apache_conf.params }
+
+  describe 'Timeout' do
+    it 'should be <= 300' do
+      expect(config['Timeout'].first.to_i).to be <= 300
+    end
+  end
+end
+describe 'configuration' do
+  let(:config) { apache_conf.params }
+
+  describe 'MaxClients' do
+    it 'should be >= 256' do
+      expect(config['MaxClients'].first.to_i).to be >= 256
+    end
+  end
+end
+describe 'configuration' do
+  let(:config) { apache_conf.params }
+
+  describe 'Listen' do
+    it 'should be = 80' do
+      expect(config['Listen'].first.to_i).to be == 80
+    end
+  end
+end
